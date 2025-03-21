803d3fdcbf574e4736316ee083d7cc31a48d2ec6
cVitorAndrade <vitorandrade1804@gmail.com>
2025-03-21 20:04:08 -0300

    feat: adjust upload function to return the upload percentage

---
+++ service/upload.service.ts
@@ -0,0 +1,23 @@
+import { Api } from "@/providers";
+
+export const UploadService = {
+  async uploadCaseFile(
+    caseId: string,
+    file: File,
+    onProgress: (progress: number) => void
+  ) {
+    const formData = new FormData();
+    formData.append("file", file);
+
+    const { data } = await Api.post(`upload/cases/${caseId}`, formData, {
+      headers: { "Content-Type": "multipart/form-data" },
+      onUploadProgress: (event) => {
+        if (event.total) {
+          const progress = Math.round((event.loaded * 100) / event.total);
+          onProgress(progress);
+        }
+      },
+    });
+    return data;
+  },
+};
