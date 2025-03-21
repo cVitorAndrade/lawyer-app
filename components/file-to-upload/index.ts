bd349a9ad13d3462c142cbd09d359bb2131194f1
cVitorAndrade <vitorandrade1804@gmail.com>
2025-03-21 20:04:45 -0300

    fix: add file to upload index

---
+++ components/file-to-upload/index.ts
@@ -0,0 +1,23 @@
+import FileToUploadDetailsSection from "./file-to-upload-details-section";
+import FileToUploadHeader from "./file-to-upload-header";
+import FileToUploadName from "./file-to-upload-name";
+import FileToUploadProgress from "./file-to-upload-progress";
+import FileToUploadRemoveButton from "./file-to-upload-remove-button";
+import FileToUploadRoot from "./file-to-upload-root";
+import FileToUploadSize from "./file-to-upload-size";
+import FileToUploadStatusIcon from "./file-to-upload-status-icon";
+import FileToUploadThumbnail from "./file-to-upload-thumbnail";
+import FileToUploadTitle from "./file-to-upload-title";
+
+export const FileToUpload = {
+  Root: FileToUploadRoot,
+  Thumbnail: FileToUploadThumbnail,
+  DetailsSection: FileToUploadDetailsSection,
+  Header: FileToUploadHeader,
+  Title: FileToUploadTitle,
+  Name: FileToUploadName,
+  Size: FileToUploadSize,
+  RemoveButton: FileToUploadRemoveButton,
+  StatusIcon: FileToUploadStatusIcon,
+  Progress: FileToUploadProgress,
+};
