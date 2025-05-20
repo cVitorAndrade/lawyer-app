32f29656c6b6be3308d9753b383632341a870a33
cVitorAndrade <vitorandrade1804@gmail.com>
2025-05-20 14:50:28 -0300

    feat: add client file schema

--- schemas/client-file/index.ts
+++ schemas/client-file/index.ts
@@ -4,3 +4,17 @@ export const uploadClientFileSchema = z.object({
   clientId: z.string().nonempty(),
   file: z.instanceof(File),
 });
+
+export const clientFileSchema = z.object({
+  id: z.string().nonempty(),
+  clientId: z.string().nonempty(),
+  uploadedById: z.string().nonempty(),
+  fullpath: z.string().nonempty(),
+  path: z.string().nonempty(),
+  mimetype: z.string().nonempty(),
+  originalname: z.string().nonempty(),
+  size: z.number(),
+  createdAt: z.string().nonempty(),
+});
+
+export type ClientFileType = z.infer<typeof clientFileSchema>;
